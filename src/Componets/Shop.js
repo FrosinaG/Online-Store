6f318c843c61fe4836d2558285e0055797d6dbaa
unknown <FrosinaG>
2023-02-13 02:09:02 +0100

    mycommit

---
+++ src/Componets/Shop.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useGetAllProductsQuery } from "../Redux/productApi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Redux/cartSlice";
+import axios from "axios";
+
+const Shop = ({}) => {
+  const { data, error, isLoading } = useGetAllProductsQuery();
+  const dispetch = useDispatch();
+  const navigate = useNavigate();
+
+  const [products, setProducts] = useState();
+  const [SearchProduct, setSerchProduct] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((response) => {
+        console.log(setProducts);
+        setSearchlist(response.data);
+      })
+      .catch((error) => {
+        console.error();
+      });
+  }, []);
+
+  const hendleAddTocart = (products) => {
+    dispetch(addToCart(products));
+    navigate("/cart");
+  };
+
+  return (
+    <div className="shop-container">
+      {isLoading ? (
+        <div className="d-flex align-items-center loading">
+          <strong>Loading...</strong>
+          <div
+            className="spinner-border ml-auto"
+            role="status"
+            aria-hidden="true"
+          ></div>
+        </div>
+      ) : error ? (
+        <p>An error occured...</p>
+      ) : (
+        <div>
+          <h2 className="h-product">
+            If shopping doesn't make you happy, then you're in the wrong shop.
+          </h2>
+          <div>
+            <div className="Search">
+              <input
+                type="text"
+                className="search-in"
+                placeholder="Search...."
+                onChange={(e) => setSerchProduct(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="row row-cols-1 row-cols-md-2 g-4 allcard">
+            {data.products
+              .filter(
+                (data) =>
+                  data.title.toLowerCase().includes(SearchProduct) ||
+                  data.brand.toLowerCase().includes(SearchProduct)
+              )
+              .map((products) => {
+                return (
+                  <div className="card cardbox" key={products.id}>
+                    <img
+                      src={products.thumbnail}
+                      className="card-img-top productsimg"
+                      alt={products.title}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{products.title}</h5>
+                      <p className="card-text">
+                        <b>Description :</b> {products.description}
+                      </p>
+
+                      <Link
+                        to={`/singelproduct/${products.id}`}
+                        className="card-link"
+                      >
+                        View more
+                      </Link>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item price">
+                        <b>Price :</b>
+                        {products.price} €
+                      </li>{" "}
+                      <button
+                        onClick={() => hendleAddTocart(products)}
+                        type="button"
+                        className="btn btn-primary addto"
+                      >
+                        {" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-cart-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                        Add To Cart
+                      </button>
+                    </ul>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Shop;
---
+++ src/Componets/SingleProducts.js
@@ -0,0 +1,112 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+
+const SingleProducts = ({ article }) => {
+  let { id } = useParams();
+  let location = useLocation();
+
+  const [product, SetProduct] = useState();
+
+  const apiCall = () => {
+    axios
+      .get(`https://dummyjson.com/products/${id}`)
+      .then((data) => {
+        SetProduct(data.data);
+      })
+      .catch(() => {
+        console.log("exception");
+      });
+  };
+  useEffect(() => {
+    apiCall();
+  }, [id]);
+
+  if (!product) return null;
+
+  return (
+    <div key={product.id} className="single-con">
+      <div className="singlebox">
+        {" "}
+        <div className="text part">
+          <p className="category">{product.category}</p>
+          <h2>{product.title}</h2>
+          <p className="card-text">
+            {product.brand}
+            <b>Description :</b> {product.description}
+          </p>
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-star-fill stars"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-star-fill stars"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-star-fill stars"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-star-fill stars"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-star-fill stars"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg>
+            {product.rating}
+          </p>
+        </div>
+        <div className="part-img">
+          {" "}
+          <img
+            src={product.thumbnail}
+            className="card-img-top productsimg singT"
+            alt="products"
+          />
+        </div>
+      </div>
+      {product.images.map((image, index) => {
+        return (
+          <div key={index} className="box">
+            <img src={image} key={index} alt="product" className="photo" />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SingleProducts;
